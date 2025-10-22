8b6b302f36eebdd6e1922acd5c9afed496d172c9
Mauno Kiiskinen <31925951+maukii@users.noreply.github.com>
2025-10-22 18:33:44 +0300

    remove logs

--- Assets/Code/PlayerFootstep.cs
+++ Assets/Code/PlayerFootstep.cs
@@ -20,20 +20,14 @@ public class PlayerFootstep : MonoBehaviour
     public void PlayStepSound()
     {
         Ray ray = new Ray(transform.position, Vector3.down);
-        if (!Physics.Raycast(ray, out RaycastHit hit, 2f))
-        {
-            Debug.Log("No hit");
-            return;
-        }
+        if (!Physics.Raycast(ray, out RaycastHit hit, 2f)) return;
+        
         string surfaceTag = hit.collider.tag;
-        Debug.Log("Hit tag: " + surfaceTag);
         FootstepSound? footstepSound = footstepSounds.Find(fs => fs.surfaceTag == surfaceTag);
-        if (footstepSound == null) 
-        {
-            Debug.Log("No sound");
-            return;
-        }
+        if (footstepSound == null) return;
 
+        // TODO::
+        // Random volume/pitch
         footstepAudioSource.PlayOneShot(footstepSound.Value.clip);
     }
 }
